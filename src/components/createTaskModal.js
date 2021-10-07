035204526fb60e67a61d93c251d7eedc91feb4b0
Nicolás Molina <nmolina@teco.com.ar>
2021-10-07 17:27:51 -0300

    Create modal for new tasks

--- src/components/createTaskModal.js
+++ src/components/createTaskModal.js
@@ -6,11 +6,14 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  ModalFooter,
+  ButtonGroup,
   FormControl,
   FormLabel,
   Input,
+  FormErrorMessage,
+  Box,
 } from "@chakra-ui/react";
+import { Formik, Form, Field } from "formik";
 
 export const CreateTaskModal = ({
   initialRef,
@@ -18,6 +21,21 @@ export const CreateTaskModal = ({
   isOpen,
   onCloseModal,
 }) => {
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = `Name is required`;
+    }
+    return error;
+  };
+
+  const validateDescription = (value) => {
+    let error;
+    if (!value) {
+      error = `Description is required`;
+    }
+    return error;
+  };
   return (
     <>
       <Modal
@@ -28,26 +46,84 @@ export const CreateTaskModal = ({
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
+          <ModalHeader>Create new task</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>First name</FormLabel>
-              <Input ref={initialRef} placeholder="First name" />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <FormLabel>Last name</FormLabel>
-              <Input placeholder="Last name" />
-            </FormControl>
+            <Formik
+              initialValues={{ name: "", description: "" }}
+              onSubmit={(values, actions) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  actions.setSubmitting(false);
+                }, 1000);
+              }}
+            >
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <Box>
+                        <FormControl
+                          isInvalid={form.errors.name && form.touched.name}
+                        >
+                          <FormLabel htmlFor="name">Name</FormLabel>
+                          <Input {...field} id="name" placeholder="name" />
+                          <FormErrorMessage>
+                            {form.errors.name}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </Box>
+                    )}
+                  </Field>
+                  <Field name="description" validate={validateDescription}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={
+                          form.errors.description && form.touched.description
+                        }
+                      >
+                        <FormLabel htmlFor="description">Description</FormLabel>
+                        <Input
+                          {...field}
+                          id="description"
+                          placeholder="Description"
+                        />
+                        <FormErrorMessage>
+                          {form.errors.description}
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="estimate">
+                    {({ field, form }) => (
+                      <FormControl>
+                        <FormLabel htmlFor="estimate">Estimate</FormLabel>
+                        <Input
+                          {...field}
+                          type="number"
+                          id="estimate"
+                          placeholder="estimate"
+                        />
+                        <FormErrorMessage>
+                          {form.errors.estimate}
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <ButtonGroup mt={10}>
+                    <Button
+                      colorScheme="teal"
+                      isLoading={props.isSubmitting}
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                    <Button onClick={onCloseModal}>Cancel</Button>
+                  </ButtonGroup>
+                </Form>
+              )}
+            </Formik>
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              Save
-            </Button>
-            <Button onClick={onCloseModal}>Cancel</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
